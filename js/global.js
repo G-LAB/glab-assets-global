505d49c29132aa13f69a3f6852b8d5f49bc732db
Ryan Brodkin <ryan@glabstudios.com>
2012-02-22 15:57:45 -0800

    Added cookie class.

--- js/global.js
+++ js/global.js
@@ -2,7 +2,37 @@
 var glab = {};
 glab.class = {};
 
+// Cookie Class
+glab.class.cookie = function () {}
+
+glab.class.cookie.prototype.set = function (name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+glab.class.cookie.prototype.get = function (name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+glab.class.cookie.prototype.erase = function (name) {
+	createCookie(name,"",-1);
+}
+
 
 /************************************************
  * AUTOLOAD CLASSES								*
  ************************************************/
+
+ glab.cookie = new glab.class.cookie();
